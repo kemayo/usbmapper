9dc1b7778396430cafcee9bd0cca4fd237546038
David Lynch <kemayo@gmail.com>
2026-03-15 15:46:33 -0500

    Convert it to an actual xcode project for distribution reasons

--- Sources/UsbMapper/UsbMapperApp.swift
+++ Sources/UsbMapper/UsbMapperApp.swift
@@ -6,13 +6,6 @@ import AppKit
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.regular)
-        // SPM doesn't produce a .app bundle, so Bundle.main has no Info.plist
-        // and macOS can't discover the icon on its own.  Load the .icns that
-        // SPM's actool generated from our xcassets and set it explicitly.
-        if let url = Bundle.module.url(forResource: "AppIcon", withExtension: "icns"),
-           let icon = NSImage(contentsOf: url) {
-            NSApp.applicationIconImage = icon
-        }
         DispatchQueue.main.async {
             NSApp.activate(ignoringOtherApps: true)
         }
